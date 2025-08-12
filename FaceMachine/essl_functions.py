13df0a7cebff998ea98080de62ed16e2d1b3c4fd
Abi0506 <abishek25052006@gmail.com>
2025-08-12 13:16:12 +0530

    Refactor device connection handling and update .gitignore; remove unused code and improve user data retrieval

--- FaceMachine/essl_functions.py
+++ FaceMachine/essl_functions.py
@@ -1,57 +1,80 @@
 import sys
-from connection import connect_to_device 
+from logs import connect_to_device 
+from connection import db
+
+
+
 
 def set_user_credentials(user_id, name):
-    conn = connect_to_device("setting user credentials")
-    
-    if not conn:
-        return "Error: Device not connected"
+    connection = db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT ip_address FROM device")
+    rows = cursor.fetchall()
 
-    try:
-        if not user_id or not name:
-            return "Error: Missing ID or name"
-        try:
-            uid = int(user_id[1:])
-        except ValueError:
-            return "Error: Invalid ID format"
+  
+    for (ip,) in rows:
         
-        password = name
-        conn.set_user(
-            uid=uid,                   
-            user_id=str(user_id),       
-            name=name,
-            privilege=0,
-            password=password
-        )
-        return f"User {user_id} added successfully"
+        conn = connect_to_device("setting user credentials",ip)
+    
+        if not conn:
+            return "Error: Device not connected"
 
-    except Exception as e:
-        return f"Error: Set credentials failed"
+        try:
+            if not user_id or not name:
+                return "Error: Missing ID or name in a device"
+            try:
+                uid = int(user_id[1:])
+            except ValueError:
+                return "Error: Invalid ID format"
+            
+            password = name
+            conn.set_user(
+                uid=uid,                   
+                user_id=str(user_id),       
+                name=name,
+                privilege=0,
+                password=password
+            )
+            return f"User {user_id} added successfully"
 
-    finally:
-        conn.disconnect()
+        except Exception as e:
+            return f"Error: Set credentials failed"
+
+        finally:
+            conn.disconnect()
+
+   
 
 def delete_user(user_id):
-    conn = connect_to_device("deleting user")
-    if not conn:
-        return "Error: Device not connected"
+    connection = db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT ip_address FROM device")
+    rows = cursor.fetchall()
 
-    try:
-        if not user_id:
-            return "Error: Missing ID"
-        try:
-            uid = int(user_id[1:])
-        except ValueError:
-            return "Error: Invalid ID format"
+  
+    for (ip,) in rows:
         
-        conn.delete_user(uid=uid)
-        return f"User {user_id} deleted successfully"
+        
+        conn = connect_to_device("deleting user",ip)
+        if not conn:
+            return "Error: Device not connected"
 
-    except Exception as e:
-        return "Error: Delete user failed"
+        try:
+            if not user_id:
+                return "Error: Missing ID in a device"
+            try:
+                uid = int(user_id[1:])
+            except ValueError:
+                return "Error: Invalid ID format"
+            
+            conn.delete_user(uid=uid)
+            return f"User {user_id} deleted successfully"
 
-    finally:
-        conn.disconnect()
+        except Exception as e:
+            return "Error: Delete user failed"
+
+        finally:
+            conn.disconnect()
 
 # def migrate():
 #     df = pd.read_excel("Faculty.xlsx")
